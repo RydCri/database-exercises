e8a029b3148164fb96b34e69c44236cba4e1e279
RydCri <107228115+RydCri@users.noreply.github.com>
2023-03-06 00:04:19 -0600

    init

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+Use zenith_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(128),
+  name VARCHAR(128),
+  release_date SMALLINT UNSIGNED,
+  sales FLOAT,
+  genre VARCHAR(128),
+  PRIMARY KEY (id)
+);
